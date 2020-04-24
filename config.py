db6aec4e1f253383e59a990114404108702b30f5
vluciano8 <vluciano88@gmail.com>
2020-04-24 19:39:57 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+SMTP_ADDRESS = "smtp.gmail.com:587"
+EMAIL_ADDRESS = "" # use your email address
+PASSWORD = "" # use your password
+DEST_ADDRESS = '' # destination mail to send
+
+""" [[yellow]]
+                     _____                _ _   _____                             
+                    |  ___|              (_) | |  ___|                            
+                    | |__ _ __ ___   __ _ _| | | |____  ___ __  _ __ ___  ___ ___ 
+                    |  __| '_ ` _ \ / _` | | | |  __\ \/ / '_ \| '__/ _ \/ __/ __|
+                    | |__| | | | | | (_| | | | | |___>  <| |_) | | |  __/\__ \__ \
+                    \____/_| |_| |_|\__,_|_|_| \____/_/\_\ .__/|_|  \___||___/___/
+                                                        | |                      
+                                                        |_|            
+[[white]]     
+
+"""
